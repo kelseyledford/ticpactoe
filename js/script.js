12fa51a0b601e4cc167fd3d8d5de93e33f5d9b5e
Kelsey Ledford <kelsey.d.ledford@gmail.com>
2014-04-24 11:34:45 -0700

    Refactoring; Changes the logo color

--- js/script.js
+++ js/script.js
@@ -3,169 +3,92 @@
 //add turn counter and count up to 9 for cat's game
 var tptApp = angular.module ('tptApp', []);
 tptApp.controller('TptController', function ($scope) { 
-	//Creates a 2D array
+	//sets the first box to appear on the front
+	$scope.box = 1;
+	//clicking the box makes a new one appear
+	$scope.clickTheBox = function () {
+		//increments the box# first, then if it reaches 4 (which doesn't exist)
+		if (++$scope.box == 4) {
+			//cycles back to the first div
+			$scope.box = 1;
+		};
+	};
+	var startSound = document.getElementById("startSound");
 	var winnerAlert = document.getElementById("winnerAlert");
 	var gameOverAlert = document.getElementById("gameOverAlert");
 	var startSound;
 	$scope.cells = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
-	//The game is not over
 	$scope.gameOver = false;
-	//It is pacman's turn to start
 	$scope.isPMTurn = true;
 	$scope.winner = null;
 	window.onload = function() {
-		startSound = document.getElementById("startSound");
 		startSound.play();
+	};
+	$scope.restart = function () {
+		$scope.cells = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
+		$scope.gameOver = false;
+		$scope.winner = null;
+		gameOverAlert.innerHTML = " ";
+		winnerAlert.innerHTML = " ";
+		startSound.play();
+	};
+	$scope.ghostDies = function() {
+		ghostDiesSound = document.getElementById("ghostDiesSound");
+		ghostDiesSound.play();
+	};
+	$scope.pacmanDies = function() {
+		pacmanDiesSound = document.getElementById("pacmanDiesSound");
+		pacmanDiesSound.play();
+	};
+	$scope.gameOverActions = function() {
+		if ($scope.isPMTurn == true) {
+			$scope.ghostDies();
+			winnerAlert.innerHTML = "PACMAN WINS!";
+			$scope.winner = "pacman";
+		//checks if ghost was the last one who played
+		} else {
+			$scope.pacmanDies();
+			winnerAlert.innerHTML = "GHOST WINS!";
+			$scope.winner = "ghost";
+		}
+		//actions that should be taken either way
+		gameOverAlert.innerHTML = "GAME  OVER";
+		$scope.gameOver = true;
+		$scope.box = 3;
 	};
 	//When a cell is clicked, this function runs with the clicked cell's index passed as a parameter
 	$scope.makeMove = function (clickedCellIndex) {
-		//If the cells is empty, check win consitions
+		//If the cells is empty, check win conditions
 		//when gameOver = true, no empty box can be clicked
 		if ($scope.gameOver == true) {
 			console.log ("Stop playing loser the game is over");
 		} else if ($scope.cells[clickedCellIndex] == ' ') {
-			console.log("you can choose this");
 			//sets cell to 1 (pacman) or -1 (ghost) depending on whose turn it is
 			$scope.cells[clickedCellIndex] = $scope.isPMTurn?1:-1;
 			//win conditions
-			//row for pacman
-			if ($scope.cells[0] + $scope.cells[1] + $scope.cells[2] == 3 ||
-				$scope.cells[3] + $scope.cells[4] + $scope.cells[5] == 3 ||
-				$scope.cells[6] + $scope.cells[7] + $scope.cells[8] == 3) {
-				var ghostDies = function() {
-					ghostDiesSound = document.getElementById("ghostDiesSound");
-					ghostDiesSound.play();
-				};
-				ghostDies();
-				gameOverAlert.innerHTML = "GAME  OVER";
-				winnerAlert.innerHTML = "PACMAN WINS!";
-				$scope.winner = "pacman";		
-				$scope.gameOver = true;
-				$scope.restart = function () {
-					$scope.cells = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
-					$scope.gameOver = false;
-					gameOverAlert.innerHTML = " ";
-					winnerAlert.innerHTML = " ";
-					startSound = document.getElementById("startSound");
-					startSound.play();
-				};
-			//row for ghost
-			} else if ($scope.cells[0] + $scope.cells[1] + $scope.cells[2] == -3 ||
-				$scope.cells[3] + $scope.cells[4] + $scope.cells[5] == -3 ||
-				$scope.cells[6] + $scope.cells[7] + $scope.cells[8] == -3) {
-				var pacmanDies = function() {
-					pacmanDiesSound = document.getElementById("pacmanDiesSound");
-					pacmanDiesSound.play();
-				};
-				pacmanDies();
-				gameOverAlert.innerHTML = "GAME  OVER";
-				winnerAlert.innerHTML = "GHOST WINS!";
-				$scope.winner = "ghost";
-				$scope.gameOver = true;
-				$scope.restart = function () {
-					$scope.cells = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
-					$scope.gameOver = false;
-					gameOverAlert.innerHTML = " ";
-					winnerAlert.innerHTML = " ";
-					startSound = document.getElementById("startSound");
-					startSound.play();
-				};
-			//column for pacman
-			} else if ($scope.cells[0] + $scope.cells[3] + $scope.cells[6] == 3 ||
-				$scope.cells[1] + $scope.cells[4] + $scope.cells[7] == 3 ||
-				$scope.cells[2] + $scope.cells[5] + $scope.cells[8] == 3) {
-				var ghostDies = function() {
-					ghostDiesSound = document.getElementById("ghostDiesSound");
-					ghostDiesSound.play();
-				};
-				ghostDies();
-				gameOverAlert.innerHTML = "GAME  OVER";
-				winnerAlert.innerHTML = "PACMAN WINS!";
-				$scope.winner = "pacman";
-				$scope.gameOver = true;
-				$scope.restart = function () {
-					$scope.cells = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
-					$scope.gameOver = false;
-					gameOverAlert.innerHTML = " ";
-					winnerAlert.innerHTML = " ";
-					startSound = document.getElementById("startSound");
-					startSound.play();
-				};
-			//column for ghost
-			} else if ($scope.cells[0] + $scope.cells[3] + $scope.cells[6] == -3 ||
-				$scope.cells[1] + $scope.cells[4] + $scope.cells[7] == -3 ||
-				$scope.cells[2] + $scope.cells[5] + $scope.cells[8] == -3) {
-				var pacmanDies = function() {
-					pacmanDiesSound = document.getElementById("pacmanDiesSound");
-					pacmanDiesSound.play();
-				};
-				pacmanDies();
-				gameOverAlert.innerHTML = "GAME  OVER";
-				winnerAlert.innerHTML = "GHOST WINS!";
-				$scope.winner = "ghost";
-				$scope.gameOver = true;
-				$scope.restart = function () {
-					$scope.cells = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
-					$scope.gameOver = false;
-					gameOverAlert.innerHTML = " ";
-					winnerAlert.innerHTML = " ";
-					startSound = document.getElementById("startSound");
-					startSound.play();
-				};
+			//checks for the absolute number (negative or positive version)
+			//rows
+			if (Math.abs($scope.cells[0] + $scope.cells[1] + $scope.cells[2]) == 3 ||
+				Math.abs($scope.cells[3] + $scope.cells[4] + $scope.cells[5]) == 3 ||
+				Math.abs($scope.cells[6] + $scope.cells[7] + $scope.cells[8]) == 3) {
+				//checks to see if pacman was the last one who clicked
+				$scope.gameOverActions();
+			//columns
+			} else if (Math.abs($scope.cells[0] + $scope.cells[3] + $scope.cells[6]) == 3 ||
+				Math.abs($scope.cells[1] + $scope.cells[4] + $scope.cells[7]) == 3 ||
+				Math.abs($scope.cells[2] + $scope.cells[5] + $scope.cells[8]) == 3) {
+				$scope.gameOverActions();
+			
 			//diagonals for pacman
-			} else if ($scope.cells[0] + $scope.cells[4] + $scope.cells[8] == 3 ||
-				$scope.cells[2] + $scope.cells[4] + $scope.cells[6] == 3) {
-				var ghostDies = function() {
-					ghostDiesSound = document.getElementById("ghostDiesSound");
-					ghostDiesSound.play();
-				};
-				ghostDies();
-				gameOverAlert.innerHTML = "GAME  OVER";
-				winnerAlert.innerHTML = "PACMAN WINS!";
-				$scope.winner = "pacman";
-				$scope.gameOver = true;
-				$scope.restart = function () {
-					$scope.cells = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
-					$scope.gameOver = false;
-					gameOverAlert.innerHTML = " ";
-					winnerAlert.innerHTML = " ";
-					startSound = document.getElementById("startSound");
-					startSound.play();
-				};
-			//diagonals for ghost
-			} else if ($scope.cells[0] + $scope.cells[4] + $scope.cells[8] == -3 ||
-				$scope.cells[2] + $scope.cells[4] + $scope.cells[6] == -3) {
-				var pacmanDies = function() {
-					pacmanDiesSound = document.getElementById("pacmanDiesSound");
-					pacmanDiesSound.play();
-				};
-				pacmanDies();
-				gameOverAlert.innerHTML = "GAME  OVER";
-				winnerAlert.innerHTML = "GHOST WINS!";
-				$scope.winner = "ghost";
-				$scope.gameOver = true;
-				$scope.restart = function () {
-					$scope.cells = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
-					$scope.gameOver = false;
-					gameOverAlert.innerHTML = " ";
-					winnerAlert.innerHTML = " ";
-					startSound = document.getElementById("startSound");
-					startSound.play();
-				};
+			} else if (Math.abs($scope.cells[0] + $scope.cells[4] + $scope.cells[8]) == 3 ||
+				Math.abs($scope.cells[2] + $scope.cells[4] + $scope.cells[6]) == 3) {
+				$scope.gameOverActions();
+				
 			//cat's game
 			} else if ($scope.cells[0] !== ' ' && $scope.cells[1] !== ' ' && $scope.cells[2] !== ' ' && $scope.cells[3] !== ' ' && $scope.cells[4] !== ' ' && $scope.cells[5] !== ' ' && $scope.cells[6] !== ' ' && $scope.cells[7] !== ' ' && $scope.cells[8]) {
 				gameOverAlert.innerHTML = "GAME  OVER";
 				winnerAlert.innerHTML = "CAT'S GAME";
-				$scope.winner = "none";
 				$scope.gameOver = true;
-				$scope.restart = function () {
-					$scope.cells = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
-					$scope.gameOver = false;
-					gameOverAlert.innerHTML = " ";
-					winnerAlert.innerHTML = " ";
-					startSound = document.getElementById("startSound");
-					startSound.play();
-				};
 			//changes to other player's turn
 			} else {
 				$scope.isPMTurn = !$scope.isPMTurn; 
@@ -176,40 +99,3 @@ tptApp.controller('TptController', function ($scope) {
 		};
 	};
 });
-
-//when clicking on cell, ifXTurn is true, puts X, if false, puts O
-//changes ifXTurn to be the opposite of current state (true or false)
-//Each player is assigned to either player1 or player2 X
-//Create a variable currentPlayer = player1
-//Create game board with 9 divs, each div has property player = null;
-//Div is clicked
-	//If div is empty (chosen = false)
-		//If currentPlayer = player1
-			//push X to that spot in the cells array
-			//player = 1;
-			//Check win conditions
-				//If row (cells w/ indexes 0,1,2 OR 3,4,5 OR 6,7,8's property player = 1)
-					//declare player1 winner
-				//else if diagonal (cells w/ indexes 0,4,8 or 2,4,6's property player = 1)
-					//declare player1 winner
-				//else if column (cells w/ indexes 0,3,6 OR 1,4,7 OR 3,5,8's property player = 1)
-					//declare player1 winner
-				//if no div has the property player = null
-					//declare cat's game
-				//else
-					//currentPlayer = player2
-		//Else
-			//push O to that spot in the cells array
-			//player = 2;
-			//Check win conditions
-				//If row (cells w/ indexes 0,1,2 OR 3,4,5 OR 6,7,8's property player = 2)
-					//declare player2 winner
-				//else if diagonal (cells w/ indexes 0,4,8 or 2,4,6's property player = 2)
-					//declare player2 winner
-				//else if column (cells w/ indexes 0,3,6 OR 1,4,7 OR 3,5,8's property player = 2)
-					//declare player2 winner
-				//if no divs have the property player = null;
-					//declare cat's game
-				//else
-					//currentPlayer = player2
-			//currentPlayer = player1
